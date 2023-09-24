a8e6c71121e68cc454f44b229397528d3c4f47b5
Ahmed <ahmed.nabil1@nomadz.dev>
2023-09-24 20:53:21 +0300

    Finished splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_sun_c9/ui/screens/home/home_screen.dart';
+import 'package:todo_sun_c9/ui/screens/splash/splash_screen.dart';
+import 'package:todo_sun_c9/ui/utils/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +13,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      routes: {
+        SplashScreen.routeName: (_) => SplashScreen(),
+        HomeScreen.routeName: (_) => HomeScreen()
+      },
+      initialRoute: SplashScreen.routeName,
+    );
   }
 }
